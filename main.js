17a5041f4412266037e73f357e04abdaf71efa4a
DownedWings <102192700+DownedWings@users.noreply.github.com>
2023-02-28 21:27:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+song = "";
+leftWristY = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristX = 0;
+
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+setup()
+{
+canvas = createCanvas(600, 500);
+canvas.center;
+
+video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+posenet.on(pose, 'gotPoses');
+}
+
+function modelLoaded(){
+    console.log('Posenet is initialized')
+}
+
+function draw(){
+    image(0,0,600,500);
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1)
+}
+
+function gotPoses(){
+    if(results.length > 0)
+    {
+    console.log(results)
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX =" + leftWristX + "leftWristY =" + leftWristY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX =" + leftWristX + "leftWristY =" + leftWristY);
+    }
+}
